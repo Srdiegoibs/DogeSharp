ba11247020db1444417d19aa77b70510f765ae1b
Luis Fernando Mariotti Paiva <fernando-mariotti@outlook.com>
2021-07-26 01:18:35 -0300

    first

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String rootPath = Paths.get("").toAbsolutePath().toString();
+        String subPath = "/src/main/java/src/";
+        String[] file = {rootPath + subPath + "scanner.flex"};
+        jflex.Main.main(file);
+    }
+
+}
